1f1477487f2d956f3c7dc54b90456efac275aac5
Tao Ning <tning@linkedin.com>
2017-11-25 08:39:17 -0500

    Add videojs into build pipeline

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -20,6 +20,10 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
+  app.import('vendor/video.js');
+  app.import('vendor/shims/video.js');
+
+  app.import('vendor/video-js.css');
 
   return app.toTree();
 };
